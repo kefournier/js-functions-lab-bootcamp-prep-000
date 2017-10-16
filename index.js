656194ae65dbeadf2cd6b922299e7bd73ce86a4d
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-10-16 00:39:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy Holidays, ${name}!"
+}
+
+function happyHolidayto(name, holiday) {
+  return "Happy ${holiday}, ${name}!"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} until ${holiday}!"
+}
